bc769f8523516cb28e7e85941a6cb37c26a4ca85
debug-gif <63322839+debug-gif@users.noreply.github.com>
2020-05-24 08:25:23 +0800

    Add files via upload

---
+++ outline.java
@@ -0,0 +1,16 @@
+
+public class outline {
+	public static void main(String[] args) {
+       int a = 600;
+       int b = 500;
+       while (a == 600)
+       {
+    	   System.out.println("ok");
+    	   a++;
+       }
+       do {
+    	   b--;
+    	   System.out.println("b=1"); 	   
+       }while (b == 400);
+    }
+}
