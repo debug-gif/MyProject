bdca8692341743db269bac6fceffc83c0e478c57
debug-gif <63322839+debug-gif@users.noreply.github.com>
2020-05-24 08:25:47 +0800

    Add files via upload

---
+++ forever.java
@@ -0,0 +1,18 @@
+
+public class forever {
+	public static void main(String[] args) {
+       int x = 100;
+	   switch(x) {
+	   case 1:
+	   System.out.println("x=1");
+	   case 5:
+		   System.out.println("x=5");
+	   case 10:
+		   System.out.println("x=10");
+	   case 100:
+		   x++;
+		   System.out.println("x=101");	   
+	   }
+	}
+
+}
