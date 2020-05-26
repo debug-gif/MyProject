a6753edb2e3e2ad418d3dc2795c33b976f2ee05e
debug-gif <63322839+debug-gif@users.noreply.github.com>
2020-05-26 11:40:13 +0800

    Add files via upload

---
+++ count.java
@@ -0,0 +1,15 @@
+
+public class count {
+	public static void main(String[] args) {
+		int x1 = 8;
+		int x2 = 10;
+		x1++;
+		x2++;
+		int result = x1 + x2;
+	    if (result >= 10) {
+	    	result++;
+	    	System.out.println(result);
+	    }
+	    }
+	}
+	   
